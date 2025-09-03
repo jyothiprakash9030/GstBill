7546aa581e38a47c4c0f1b520870ec0cafe250ab
jyothiprakash9030 <jyothiprakash9076@gmail.com>
2025-09-03 14:39:18 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "Nama Tea World",
+  title: "Namma Tea World",
   description: "Tea Shop Billing Software",
   generator: "Billing software",
 }
